6a6b093ca0251cc00e2634eaf913893f3313d8da
CYNahko <cynthiayasutake@gmail.com>
2024-09-03 10:35:54 -0300

    aleatorio

--- primeiro_flask.py
+++ primeiro_flask.py
@@ -5,6 +5,8 @@ app = Flask(__name__)
 
 
 alunos = []
+disciplinas = []
+matriculas = []
 
 @app.route('/', methods=['GET'])
 def joelho():
@@ -21,30 +23,80 @@ def teste():
 
     return 'Web service funcionando', 200
 
-@app.route('/cadastro_aluno', methods=['POST'])
+@app.route('/aluno', methods=['POST'])
 def cadastro():
 
     # entrada do usuário
     entrada_dados = request.json
+    entrada_dados_dict = dict(entrada_dados)
 
-    # usar caso algo não esteja funcionando
-    #breakpoint()
-
-    # adiciona o aluno na lista
-    alunos.append(entrada_dados)
+    # verifica se as chaves foram digitadas corretamente
+    if entrada_dados_dict["nome"] not in entrada_dados:
+        return "Chave inválida. O correto é 'nome'",400
+    elif entrada_dados_dict["idade"] not in entrada_dados:
+        return "Chave inválida. O correto é 'idade'",400
+    elif entrada_dados_dict["cpf"] not in entrada_dados:
+        return "Chave inválida. O correto é 'cpf'"
+    else:
+        entrada_dados['id'] = len(alunos)+1
+        # adiciona o aluno na lista
+        alunos.append(entrada_dados)
 
     #resposta para o usuário
     resp = "Aluno cadastrado com sucesso"
 
     return resp, 201    
 
-@app.route('/lista_alunos', methods=['GET'])
+@app.route('/aluno', methods=['GET'])
 
 def lista_alunos():
     resp = {
         "alunos": alunos
     }
     return resp, 200
+
+
+@app.route('/aluno/<int:id>', methods=['DELETE'])
+def deletar_aluno(id):
+    id_aluno = id
+    for aluno in alunos:
+        if aluno['id'] == id_aluno:
+            alunos.remove(aluno)
+            return "Aluno deletado com sucesso.",200
+
+@app.route('/aluno/<int:id>', methods=['GET'])
+def buscar_aluno(id):
+    for aluno in alunos:
+        if aluno['id'] == id:
+            return aluno
+        
+@app.route('/aluno/<int:id>', methods=['PUT'])
+def atualizar_aluno(id):
+    atualizado = request.json
+    if alunos == []:
+        return "ID inexistente", 400
+    else:
+        for aluno in alunos:
+            if id == aluno["id"]:
+                aluno["nome"] = atualizado["nome"]
+                aluno["idade"]  = atualizado["idade"]
+                aluno["cpf"] = atualizado["cpf"]
+                return "Dados atualizados com sucesso.",200
+
+@app.route('/disciplina', methods=['GET'])
+def lista_disciplinas():
+    resp = {
+        "disciplinas": disciplinas
+    }
+    return resp, 200
+
+@app.route('/disciplina',methods=['POST'])
+def cadastro_disciplina():
+    entrada_dados = request.json
+    disciplina_id = len(disciplinas) + 1
+
+    
+
 
 
 if __name__ == '__main__':

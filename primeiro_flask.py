2706dd26600bd419621b64094ebee7cd93ece47f
CYNahko <cynthiayasutake@gmail.com>
2024-09-07 10:09:21 -0300

    Aplicar REST e funcionalidades da disciplina e matrícula

--- primeiro_flask.py
+++ primeiro_flask.py
@@ -28,19 +28,16 @@ def cadastro():
 
     # entrada do usuário
     entrada_dados = request.json
-    entrada_dados_dict = dict(entrada_dados)
 
     # verifica se as chaves foram digitadas corretamente
-    if entrada_dados_dict["nome"] not in entrada_dados:
-        return "Chave inválida. O correto é 'nome'",400
-    elif entrada_dados_dict["idade"] not in entrada_dados:
-        return "Chave inválida. O correto é 'idade'",400
-    elif entrada_dados_dict["cpf"] not in entrada_dados:
-        return "Chave inválida. O correto é 'cpf'"
-    else:
-        entrada_dados['id'] = len(alunos)+1
-        # adiciona o aluno na lista
-        alunos.append(entrada_dados)
+    chaves = ['nome', 'idade', 'cpf']
+    for chave in chaves:
+        if chave not in entrada_dados.keys():
+            resp = 'Chave incorreta, o correto é:',{chave}
+            return resp,400
+    entrada_dados['id'] = len(alunos)+1
+    # adiciona o aluno na lista
+    alunos.append(entrada_dados)
 
     #resposta para o usuário
     resp = "Aluno cadastrado com sucesso"
@@ -68,7 +65,8 @@ def deletar_aluno(id):
 def buscar_aluno(id):
     for aluno in alunos:
         if aluno['id'] == id:
-            return aluno
+            return aluno,200
+    return 'Aluno não encontrado',400
         
 @app.route('/aluno/<int:id>', methods=['PUT'])
 def atualizar_aluno(id):
@@ -93,11 +91,83 @@ def lista_disciplinas():
 @app.route('/disciplina',methods=['POST'])
 def cadastro_disciplina():
     entrada_dados = request.json
+    chaves = ['nome', 'quantidade de aulas por semana', 'curso']
+    for chave in chaves:
+        if chave not in entrada_dados.keys():
+            resp = 'Chave incorreta, o correto é:',{chave}
+            return resp,400
     disciplina_id = len(disciplinas) + 1
+    entrada_dados['id'] = disciplina_id
+    disciplinas.append(entrada_dados)
+    return "Disciplina cadastrada com sucesso!", 200
 
-    
+@app.route('/disciplina/<int:id>',methods=['DELETE'])
+def deletar_disciplina(id):
+    for disciplina in disciplinas:
+        if disciplina['id'] == id:
+            disciplinas.remove(disciplina)
+            return 'Disciplina deletada com sucesso.', 200
+        
+@app.route('/disciplina/<int:id>',methods=['GET'])
+def busca_disciplina(id):
+    for disciplina in disciplinas:
+        if disciplina['id'] == id:
+            return disciplina,200
+    return 'Disciplina não encontrada.', 400
 
+@app.route('/disciplina/<int:id>',methods=['PUT'])
+def atualiza_disciplina(id):
+    atualiza_dados = request.json
+    for disciplina in disciplinas:
+        if disciplina['id'] == id:
+            disciplina['nome'] = atualiza_dados['nome']
+            disciplina['quantidade de aulas por semana'] = atualiza_dados['quantidade de aulas por semana']
+            disciplina['curso'] = atualiza_dados['curso']
+            return 'Dados atualizados com sucesso.', 200
 
+@app.route('/matricula',methods=['GET'])
+def lista_matriculas():
+    resp = {
+        'matriculas': matriculas
+    }
+    return resp, 200
+
+@app.route('/matricula', methods=['POST'])
+def criar_matricula():
+    entrada_dados = request.json
+    chaves = ['id_aluno', 'id_disciplina']
+    # verifica se as chaves existem
+    for chave in chaves:
+        if chave not in entrada_dados.keys():
+            resp = 'Chave incorreta, o correto é:',{chave}
+            return resp,400
+    # após confirmar que existem, verificam por id_aluno
+    for aluno in alunos:
+        if entrada_dados['id_aluno'] == aluno['id']:
+            if chaves[1] in entrada_dados.keys():
+                for disciplina in disciplinas:
+                    if entrada_dados['id_disciplina'] == disciplina['id']:
+                        id_matricula = len(matriculas) + 1
+                        entrada_dados['id'] = id_matricula
+                        matriculas.append(entrada_dados)
+                        resp = 'Matricula cadastrada com sucesso.'
+                        return resp, 201
+                return 'ID de disciplina inexistente.',400
+                    
+@app.route('/matricula/<int:id_aluno>',methods=['GET'])
+def buscar_matricula(id_aluno):
+    aluno_matriculado = []
+    for matricula in matriculas:
+        if matricula['id_aluno'] == id_aluno:
+            aluno_matriculado.append(matricula)
+    return aluno_matriculado, 200
+
+@app.route('/matricula/<int:id>',methods=['DELETE'])
+def deletar_matricula(id):
+    for matricula in matriculas:
+        if matricula['id'] == id:
+            matriculas.remove(matricula)
+            return 'Matricula deletada com sucesso.',200
 
 if __name__ == '__main__':
     app.run(debug=True)
